5365988663a13e7531f8af1190ef846d14b7876d
Fred <akamaatei@gmail.com>
2020-11-01 22:44:26 +0300

    add navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,9 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from . import main
+# from flask_login import login_required,current_user
+# from ..models import User,Pitch,Comment,Upvote,Downvote
+# from .forms import UpdateProfile,AddPitch,CommentForm,UpvoteForm,Downvote
+# from .. import db,photos
 from flask.views import View,MethodView
 
 @main.route('/')
@@ -9,6 +13,6 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    title = 'Blog'
+    title = 'SafeSpace'
     
     return render_template('index.html', title = title)
