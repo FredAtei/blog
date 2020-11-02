d603bcd8d2b025dec414be7706495a27a8921a42
Fred <akamaatei@gmail.com>
2020-11-02 13:01:31 +0300

    add config and manage file db

--- manage.py
+++ manage.py
@@ -9,6 +9,9 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 @manager.command
 def test():
