bcaa5ab720ac6a0ba8203ea0a6908741f7730b69
Fred <akamaatei@gmail.com>
2020-11-02 16:57:31 +0300

    add email and modify profile

--- app/main/views.py
+++ app/main/views.py
@@ -6,18 +6,15 @@ from flask_login import login_required,current_user
 from .. import db,photos
 
 @main.route('/')
-@login_required
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
 
+    title = 'Home'
 
-    title = 'Home - Shitandi blog wbsite'
-    content = "WELCOME TO SHITANDI BLOG WEBSITE"
-
-    return render_template('index.html', title = title,content = content)
+    return render_template('index.html', title = title)
 
 
 
@@ -29,3 +26,33 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)    
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))    
--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 import os
+
 class Config:
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY') 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     
