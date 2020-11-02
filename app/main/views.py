4f56fe9489594de98ab3c89cad079bf9514cff60
Fred <akamaatei@gmail.com>
2020-11-02 13:00:19 +0300

    add files and details

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,31 @@
-from flask import render_template,request,redirect,url_for,abort,flash
+from flask import render_template,request,redirect,url_for,abort
 from . import main
-# from flask_login import login_required,current_user
-# from ..models import User,Pitch,Comment,Upvote,Downvote
-# from .forms import UpdateProfile,AddPitch,CommentForm,UpvoteForm,Downvote
-# from .. import db,photos
-from flask.views import View,MethodView
+from .forms import UpdateProfile
+from ..models import User
+from flask_login import login_required,current_user
+from .. import db,photos
 
 @main.route('/')
+@login_required
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    
-    title = 'SafeSpace'
-    
-    return render_template('index.html', title = title)
+
+
+    title = 'Home - Shitandi blog wbsite'
+    content = "WELCOME TO SHITANDI BLOG WEBSITE"
+
+    return render_template('index.html', title = title,content = content)
+
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
