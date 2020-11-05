db20c8a433e6d237e4a1e6ac62e2329ea48f3514
Fred <akamaatei@gmail.com>
2020-11-05 16:59:25 +0300

    modify

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/bloge'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogs'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -18,7 +18,7 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
     
 class DevConfig(Config):
